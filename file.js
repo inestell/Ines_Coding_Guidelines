5f3a2ab86c1ca4ee3daeff4785538739f895054a
Ines <Ines@a88-157-238-17.static.cpe.netcabo.pt>
2024-05-31 09:51:38 +0100

    Ugly and Beatiful

---
+++ file.js
@@ -0,0 +1,16 @@
+// Ugly Code
+function f(a, b) {
+    x = a + b;
+    return x
+}
+
+console.log(f(3,5))
+
+// Beautiful Code
+function sum (num1, num2) {
+    let numSum = num1 + num2;
+    return numSum
+}
+
+let result = sum(3,5);
+console.log(result);
